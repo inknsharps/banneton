07a9a1acff68c51567fb88824ced8a7965a666df
inknsharps <nchen25@gmail.com>
2021-06-22 21:55:25 -0400

    add user and post schema

---
+++ server/models/Post.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    author: {
+        type: String,
+        default: "author",
+        required: true
+    },
+    content: {
+        type: String,
+        default: "comment content",
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const postSchema = new Schema({
+    author: {
+        type: String,
+        default: "author",
+        required: true
+    },
+    title: {
+        type: String,
+        default: "title",
+        required: true
+    },
+    ingredients: {
+        type: Array,
+        default: ["list", "ingredients", "here"],
+        required: true
+    },
+    method: {
+        type: String,
+        default: "method",
+        required: true
+    },
+    tags: {
+        type: Array,
+        default: ["list", "tags", "here"]
+    },
+    comments: {
+        type: [commentSchema],
+        default: []
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
---
+++ server/models/User.js
@@ -0,0 +1,58 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: "Username is Required!"
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: "Password is Required!",
+        validate: [({ length }) => length >= 6, "Password must be 6 characters or more."]
+    },
+    email: {
+        type: String,
+        unique: true,
+        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    }
+});
+
+userSchema.pre("save", function(next) {
+    const user = this;
+    if (!user.isModified("password")){
+        return next();
+    };
+    try {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        user.password = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+userSchema.pre("updateOne", function(next) {
+    const update = this.getUpdate().$set;
+    if (!update.password) {
+        return next();
+    };
+    try {
+        const hashedPassword = await bcrypt.hash(update.password, 10);
+        update.password = hashedPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+userSchema.methods.comparePassword = function(inputtedPass) {
+    return bcrypt.compareSync(inputtedPass, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
