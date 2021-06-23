7d64b442c7436e44f98681f0a9c0059c7b72ddd3
inknsharps <nchen25@gmail.com>
2021-06-23 00:35:19 -0400

    add base comment routing

--- server/routes/api/commentRoutes.js
+++ server/routes/api/commentRoutes.js
@@ -2,22 +2,33 @@ const router = require("express").Router();
 const Post = require("../../models/Post");
 const cors = require("cors");
 
-// Unsure if this works, basically we pass in the _id of the comment, then update the Post directly (since the comment is a subdocument). 
-// Our matcher is for the comment _id, then we use the $pull operator to remove the entry with that _id from the array of comments.
-router.delete("/:id", cors(), async (res, req) => {
+// The _id that should be passed here is the Post's _id.
+// We use the push operator to push the new comment to the Post's subdocument.
+// Make sure that the comment value is in the format of the comment schema.
+router.put("/:id", cors(), async (req, res) => {
+	try {
+		const updatedPost = await Post.updateOne(
+			{ _id: req.params.id },
+			{ $push: { comments: req.body.comments } }
+		);
+		res.json(updatedPost);
+	} catch (error) {
+        res.status(500).json(error);
+	};
+});
+
+// In the req object, provide a postId and a commentId key-value pair.
+// Then we update the post based on its _id, then use the $pull operator to remove an entry in the comments subdocument (which is an array), where the comment _id matches the commentId in the req body.
+router.delete("/", cors(), async (req, res) => {
     try {
         const deletedComment = await Post.updateOne(
-            { 
-                [comment._id]: req.params.id 
-            },
-            {
-                $pull: { comment: { _id: req.params.id }
-            }
-        });
+            { _id: req.body.postId },
+            { $pull: { comments: { _id: req.body.commentId } } },
+        );
         res.json(deletedComment);
     } catch (error) {
         res.status(500).json(error);
-    }
+    };
 });
 
 module.exports = router;
