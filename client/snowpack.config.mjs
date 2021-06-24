3ce4c70d04a04e90ade33a01b4e3d60d4e1b0039
inknsharps <nchen25@gmail.com>
2021-06-24 00:15:26 -0400

    add initial configs for tailwind css

--- client/snowpack.config.mjs
+++ client/snowpack.config.mjs
@@ -1,27 +1,25 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-  mount: {
-    public: '/',
-    src: '/dist',
-  },
-  plugins: [
-    /* ... */
-  ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+    mount: {
+        public: "/",
+        src: "/dist"
+    },
+    plugins: ["@snowpack/plugin-postcss"],
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    ],
+    optimize: {
+        /* Example: Bundle your final build: */
+        // "bundle": true,
+    },
+    packageOptions: {
+        /* ... */
+    },
+    devOptions: {
+        tailwindConfig: "./tailwind.config.js"
+    },
+    buildOptions: {
+        /* ... */
+    }
 };
