6950b8b25db374119ab3f7e12dcb6348a4edc370
inknsharps <nchen25@gmail.com>
2021-06-27 22:44:56 -0400

    update form inputs to have a value prop

--- client/src/components/Form/FormInput/FormInput.jsx
+++ client/src/components/Form/FormInput/FormInput.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const FormInput = ({ labelName, htmlFor, inputType = "text", placeholder = "" }) => {
+const FormInput = ({ labelName, htmlFor, inputType = "text", placeholder = "", value = "" }) => {
     return (
         <div className="FormInput">
             <label className="p-1" htmlFor={ htmlFor }>{ labelName }</label>
             <br />
-            <input className="border-2 text-center" type={ inputType } name={ htmlFor } placeholder={ placeholder }></input>
+            <input className="border-2 text-center" type={ inputType } name={ htmlFor } placeholder={ placeholder } value={ value }></input>
         </div>
     )
 };
