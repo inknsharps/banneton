05426a8b3ad9f8018bb39e57785a0d4d702d1a7e
inknsharps <nchen25@gmail.com>
2021-07-08 00:53:31 -0400

    add custom animation utility class

--- client/src/components/Navbar/NavbarContainer/NavbarContainer.jsx
+++ client/src/components/Navbar/NavbarContainer/NavbarContainer.jsx
@@ -13,25 +13,36 @@ const NavbarContainer = () => {
     const toggleNavExpand = navState => {
         setNavToggle(!navState);
     };
+
+    const handleLinkClick = () => {
+        setNavToggle(false);
+    };
     
     return (
-        <nav className="NavbarContainer flex justify-between bg-indigo-600 text-white">
-            <NavLink to="/">
-                <h1 className="p-5 text-3xl">Banneton</h1>
-            </NavLink>
-            <a className="self-center" href="javascript:void(0);" onClick={ () => toggleNavExpand(navToggle) }>
-                <i className="fas fa-bars fa-2x"></i>
-            </a>
-            <ul className="p-5 flex justify-evenly">
-                { !userState.loggedIn ? <NavbarLi name="Login" link="/login"/> : <NavbarLi name="Logout" link="/logout" /> }
-            </ul>
-            <ul className="p-5 flex justify-evenly">
-                <NavbarLi name="Search" link="/search"/>
-            </ul>
-            <ul className="p-5 flex justify-evenly">
-                { userState.loggedIn ? <NavbarLi name="Dashboard" link="/dashboard" /> : null }
-            </ul>
-        </nav>
+        <>
+            <nav className="NavbarContainer flex justify-between bg-indigo-600 text-white">
+                <a className="self-center cursor-pointer active:animate-spin-fast p-5" onClick={ () => toggleNavExpand(navToggle) }>
+                    <i className="fas fa-bars fa-2x"></i>
+                </a>
+                <NavLink to="/">
+                    <h1 className="p-5 text-3xl">Banneton</h1>
+                </NavLink>
+            </nav>
+            { navToggle 
+                ? <div className="fixed text-white bg-indigo-600">
+                    <ul className="flex justify-evenly px-24">
+                        { !userState.loggedIn ? <NavbarLi name="Login" link="/login" onClick={ () => handleLinkClick } /> : <NavbarLi name="Logout" link="/logout" /> }
+                    </ul>
+                    <ul className="flex justify-evenly">
+                        <NavbarLi name="Search" link="/search"/>
+                    </ul>
+                    <ul className="flex justify-evenly">
+                        { userState.loggedIn ? <NavbarLi name="Dashboard" link="/dashboard" /> : null }
+                    </ul>
+                </div>
+                : null
+            }
+        </>
     )
 };
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,4 +2,14 @@ module.exports = {
     // mode: "jit",
     purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
     // specify other options here
+    theme: {
+        extend: {
+            animation: {
+                "spin-fast": "spin 300ms linear",
+            }
+        }
+    },
+    variants: {
+        animation: ["active"],
+    }
 };
