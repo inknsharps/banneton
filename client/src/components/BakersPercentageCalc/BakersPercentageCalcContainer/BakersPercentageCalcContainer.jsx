ed4bebae20dfd75787fea1d49defd3f519b1dc16
inknsharps <nchen25@gmail.com>
2021-07-08 21:20:58 -0400

    add ability to add your own custom ingredients and amounts

--- client/src/components/BakersPercentageCalc/BakersPercentageCalcContainer/BakersPercentageCalcContainer.jsx
+++ client/src/components/BakersPercentageCalc/BakersPercentageCalcContainer/BakersPercentageCalcContainer.jsx
@@ -8,20 +8,30 @@ const BakersPercentageCalcContainer = () => {
     const [ flourWeight, setFlourWeight ] = useState(0);
     const [ ingredients, setIngredients ] = useState([]);
 
-    const addIngredient = (currentValues, newValue) => {
+    const addIngredient = (currentValues, name, value) => {
+        const newValue = {
+            name: name,
+            value: value
+        };
         setIngredients([...currentValues, newValue]);
     };
 
     const generateIngredientInputs = ingredients => {
-        return ingredients.map(ingredient => <IngredientInput key={ ingredient } labelName={ ingredient } />)
+        return ingredients.map(ingredient => <IngredientInput key={ ingredient.name } labelName={ ingredient.name } defaultValue={ ingredient.value } />)
     };
 
     return (
-        <BakersPercentageContext.Provider value={ { flourWeight, setFlourWeight } }>
+        <BakersPercentageContext.Provider value={ { flourWeight, setFlourWeight, ingredients } }>
             <div className="BakersPercentageCalcContainer">
                 <FlourInput value={ flourWeight } />
                 { generateIngredientInputs(ingredients) }
-                <button onClick={ () => addIngredient(ingredients, "potato") }>Add Ingredient!</button>
+                <form onSubmit={ event => addIngredient(ingredients, event.target[0].value, event.target[1].value) }>
+                <label>Ingredient Name</label>
+                <input></input>
+                <label>Ingrdient Amount</label>
+                <input></input>
+                <button type="submit">Add Ingredient!</button>
+                </form>
             </div>
         </BakersPercentageContext.Provider>
     )
--- client/src/components/BakersPercentageCalc/IngredientInput/IngredientInput.jsx
+++ client/src/components/BakersPercentageCalc/IngredientInput/IngredientInput.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const IngredientInput = ({ labelName }) => {
+const IngredientInput = ({ labelName, defaultValue }) => {
     return (
         <div className="IngredientInput">
             <label>{ labelName }</label>
-            <input className="border-2 border-gray-200" defaultValue={0} onChange={ event => console.log(event.target.value) }></input>
+            <input className="border-2 border-gray-200" defaultValue={ defaultValue } onChange={ event => console.log(event.target.value) }></input>
         </div>
     )
 };
