b5c9508a82ac1cd66a04443d35aee668167532c3
inknsharps <nchen25@gmail.com>
2021-07-10 11:38:34 -0400

    update hero container for mobile responsiveness

--- client/src/components/Hero/HeroContainer/HeroContainer.jsx
+++ client/src/components/Hero/HeroContainer/HeroContainer.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const HeroContainer = () => {
     return (
-        <div className="HeroContainer flex justify-center border-b-4 border-gray-300 py-44">
-            <div className="ml-36">
-                <img className="absolute bottom-1/2 left-72 z-0 m-5 w-1/4 h-1/4" src="./banneton.svg"></img>
-                <h1 className="relative z-10 text-9xl">Banneton.</h1>
-                <h2 className="relative z-10 text-3xl">Social media for the ardent home baker.</h2>   
+        <div className="HeroContainer flex justify-center border-b-4 border-gray-300 py-12 lg:py-44">
+            <div className="flex flex-col items-center justify-center lg:ml-36">
+                <img className="w-1/2 h-1/2 lg:m-5" src="./banneton.svg"></img>
+                <h1 className="relative z-10 text-3xl lg:text-9xl">Banneton.</h1>
+                <h2 className="relative z-10 text-xl">Social media for the ardent home baker.</h2>   
             </div>
         </div>
     )
