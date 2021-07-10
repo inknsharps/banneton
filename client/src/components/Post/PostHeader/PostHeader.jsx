8c10ecd723d68caa220e1c85a0d91a1941acd81c
inknsharps <nchen25@gmail.com>
2021-07-10 00:24:45 -0400

    update styling on post header

--- client/src/components/Post/PostHeader/PostHeader.jsx
+++ client/src/components/Post/PostHeader/PostHeader.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 const PostHeader = ({ title, author, date }) => {
     return (
-        <div className="PostHeader p-5 border-b-2 border-t-2 border-gray-300 flex justify-between items-center">
-            <svg className="relative z-0" height="150" width="500">
-                <polygon className="text-purple-300 fill-current" points="0,0 200,0, 0,150"/>
-                <polygon className="text-indigo-300 fill-current" points="0,0 300,0, 0,100"/>
-            </svg>
-            <div className="absolute top-42 left-32">
-                <h2 className="text-7xl">{ title }</h2>
+        <div className="PostHeader border-b-2 border-t-2 border-gray-300 p-5">
+            <div className="flex flex-row justify-between items-center bg-indigo-500 rounded-xl text-white px-4 py-2">
+                <div className="p-5">
+                    <h2 className="text-7xl">{ title }</h2>
+                </div>
+                <h3 className="italic self-end justify-self-end">Posted by: { author } on { date }</h3>
             </div>
-            <h3 className="italic self-end">Posted by: { author } on { date }</h3>
         </div>
     )
 };
