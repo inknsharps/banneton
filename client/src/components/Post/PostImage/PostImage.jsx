c025aa54b57d9d31b32b1df3095129402a3b04e2
inknsharps <nchen25@gmail.com>
2021-07-11 10:57:32 -0400

    update post page styling for mobile responsiveness

--- client/src/components/Post/PostImage/PostImage.jsx
+++ client/src/components/Post/PostImage/PostImage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PostImage = ({ image, title }) => {
     return (
         <div className="PostImage m-5 row-span-3 border-2 rounded-md flex flex-col shadow-md">
-            <img className="m-10 rounded-md shadow-md" src={ image } alt={ title } />
+            <img className="lg:m-10 rounded-md shadow-md" src={ image } alt={ title } />
         </div>
     )
 };
