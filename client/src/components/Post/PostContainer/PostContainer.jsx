35987d1aa5e2e93b59892564bf16b1869c10552c
inknsharps <nchen25@gmail.com>
2021-07-11 10:45:26 -0400

    add useViewport hook, post container responsiveness

--- client/src/components/Post/PostContainer/PostContainer.jsx
+++ client/src/components/Post/PostContainer/PostContainer.jsx
@@ -6,17 +6,29 @@ import PostIngredients from "../PostIngredients/PostIngredients";
 import PostMethod from "../PostMethod/PostMethod";
 import PostTags from "../PostTags/PostTags";
 
+import useViewport from "../../../hooks/useViewport";
+
 // Pass in all the post information from the getSinglePost API call
 const PostContainer = ({ author, date, image, ingredients, method, tags, title }) => {
+    const [ isDesktop ] = useViewport(1024);
+
     return (
         <div className="PostContainer lg:m-10 lg:p-10 border-4 border-gray-300 border-double rounded-md shadow-xl">
             <PostHeader author={ author } title={ title } date={ date } />
-            <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-3 bg-gray-100 bg-opacity-50 border-2 border-gray-200 rounded-xl m-5">
-                <PostIngredients ingredients={ ingredients } />
-                <PostImage image={ image } title={ title } />
-                <PostMethod method={ method } />
-                <PostTags tags={ tags } />
-            </div>
+            { isDesktop 
+                ? <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-3 bg-gray-100 bg-opacity-50 border-2 border-gray-200 rounded-xl m-5">
+                    <PostIngredients ingredients={ ingredients } />
+                    <PostImage image={ image } title={ title } />
+                    <PostMethod method={ method } />
+                    <PostTags tags={ tags } />
+                </div>
+                : <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-3 bg-gray-100 bg-opacity-50 border-2 border-gray-200 rounded-xl m-5">
+                    <PostImage image={ image } title={ title } />
+                    <PostIngredients ingredients={ ingredients } />
+                    <PostMethod method={ method } />
+                    <PostTags tags={ tags } />
+                </div>
+            }
         </div>
     )
 };
