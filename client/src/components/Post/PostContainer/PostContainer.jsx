da1d7f8e93f51097d17e34ef2b14a263658ed1d3
inknsharps <nchen25@gmail.com>
2021-07-10 23:27:44 -0400

    update post styling for responsiveness

--- client/src/components/Post/PostContainer/PostContainer.jsx
+++ client/src/components/Post/PostContainer/PostContainer.jsx
@@ -9,9 +9,9 @@ import PostTags from "../PostTags/PostTags";
 // Pass in all the post information from the getSinglePost API call
 const PostContainer = ({ author, date, image, ingredients, method, tags, title }) => {
     return (
-        <div className="PostContainer m-10 p-10 border-4 border-gray-300 border-double rounded-md shadow-xl">
+        <div className="PostContainer lg:m-10 lg:p-10 border-4 border-gray-300 border-double rounded-md shadow-xl">
             <PostHeader author={ author } title={ title } date={ date } />
-            <div className="grid grid-cols-2 grid-rows-3 bg-gray-100 bg-opacity-50 border-2 border-gray-200 rounded-xl m-5">
+            <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-3 bg-gray-100 bg-opacity-50 border-2 border-gray-200 rounded-xl m-5">
                 <PostIngredients ingredients={ ingredients } />
                 <PostImage image={ image } title={ title } />
                 <PostMethod method={ method } />
--- client/src/components/Post/PostHeader/PostHeader.jsx
+++ client/src/components/Post/PostHeader/PostHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PostHeader = ({ title, author, date }) => {
     return (
         <div className="PostHeader border-b-2 border-t-2 border-gray-300 p-5">
-            <div className="flex flex-row justify-between items-center bg-indigo-500 rounded-xl text-white px-4 py-2">
+            <div className="flex flex-col lg:flex-row justify-between items-center bg-indigo-500 rounded-xl text-white px-4 py-2">
                 <div className="p-5">
                     <h2 className="text-7xl">{ title }</h2>
                 </div>
