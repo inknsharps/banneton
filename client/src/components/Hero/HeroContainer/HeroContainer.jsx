a341c82caf53047e70c34e6d37e80567e08e3da2
inknsharps <nchen25@gmail.com>
2021-07-12 20:11:15 -0400

    update hero container

--- client/src/components/Hero/HeroContainer/HeroContainer.jsx
+++ client/src/components/Hero/HeroContainer/HeroContainer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const HeroContainer = () => {
     return (
-        <div className="HeroContainer flex justify-center border-b-4 border-gray-300 py-12 lg:py-44">
-            <div className="flex flex-col items-center justify-center lg:ml-36">
+        <div className="HeroContainer flex justify-center border-b-4 border-gray-300 py-12 lg:py-12">
+            <div className="flex flex-col items-center justify-center">
                 <img className="w-1/2 h-1/2 lg:m-5" src="./banneton.svg"></img>
                 <h1 className="relative z-10 text-3xl lg:text-9xl">Banneton.</h1>
                 <h2 className="relative z-10 text-xl">Social media for the ardent home baker.</h2>   
