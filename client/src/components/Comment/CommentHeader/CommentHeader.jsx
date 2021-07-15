c23f730071c9f83ed1ca778d1d16a39b9d1a4a5b
inknsharps <nchen25@gmail.com>
2021-07-15 00:46:29 -0400

    add user links for components

--- client/src/components/Comment/CommentHeader/CommentHeader.jsx
+++ client/src/components/Comment/CommentHeader/CommentHeader.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const CommentHeader = ({ author = "author", date ="date" }) => {
+const CommentHeader = ({ author = "author", date ="date", authorId }) => {
     return (
         <div className="CommentHeader rounded-t-xl bg-indigo-300 bg-opacity-50 p-5">
-            <h4 className="bottom-0 left-0 text-xl font-thin">Comment from { author } on { date }:</h4>
+            <h4 className="bottom-0 left-0 text-xl font-thin">Comment from <a href={`/#/user/${authorId}`}>{ author }</a> on { date }:</h4>
         </div>
     )
 };
